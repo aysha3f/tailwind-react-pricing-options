90d5e73b6c4860fecf86877dae36b851115c965e
aysha3f <tahiaysha220@gmail.com>
2025-11-01 07:23:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+  <a className="btn btn-ghost text-xl">daisyUI</a>
+</div>
+      <h1 className='text-6xl'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
