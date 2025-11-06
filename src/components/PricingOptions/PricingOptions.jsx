8f396413dd64e549f3d1b1062353c6309060f533
aysha3f <tahiaysha220@gmail.com>
2025-11-06 20:06:51 +0600

    Use Daisy UI pricing card to modify for the component

--- src/components/PricingOptions/PricingOptions.jsx
+++ src/components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../pricingcard/PricingCard';
+import DaisyPricingCard from '../DaisyPricingCard/DaisyPricingCard';
 
 const PricingOptions = ({pricingPromise}) => {
   
@@ -9,12 +10,18 @@ const PricingOptions = ({pricingPromise}) => {
      return (
         <div>
            <h2 className='text-5xl'>Get our Membership</h2> 
-           <div className='grid md:grid-cols-3 gap-8'>
-            {
+           <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+            {/* {
                 pricingData.map((pricing) => <PricingCard
                     key={pricing.id}
                     pricing={pricing}></PricingCard>)
-            }
+            } */}
+
+           {
+            pricingData.map(pricing => <DaisyPricingCard 
+                key={pricing.id}
+                pricing={pricing}></DaisyPricingCard>)
+           }
            </div>
         </div>
     );
--- src/components/pricingcard/PricingCard.jsx
+++ src/components/pricingcard/PricingCard.jsx
@@ -4,20 +4,21 @@ import PricingFeatures from './pricingFeatures';
 const PricingCard = ({pricing}) => {
    const {name, price , features} = pricing;
     return (
-        <div className='border p-4 bg-amber-600 rounded-2xl text-white'>
+        <div className=' flex flex-col border p-4 bg-amber-600 rounded-2xl text-white'>
            {/* card header  */}
            <div>
             <h1 className='text-7xl'>{name}</h1>
             <h4 className='text-3xl'>{price}</h4>
            </div>
            {/* card body */}
-           <div className='bg-amber-400 p-4 rounded-2xl mt-10'>
+           <div className='bg-amber-400 p-4 rounded-2xl mt-10 flex-1'>
         {
             features.map((feature, index) => <PricingFeatures 
                 key={index}
             feature={feature}></PricingFeatures>)
         }
            </div>
+           <button className="btn w-full mt-4">Subscribe</button>
         </div>
     );
 };
